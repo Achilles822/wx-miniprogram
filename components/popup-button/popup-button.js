a91adc472590e774167646212c7096dcca32ebb2
YAN ZIYU <779239635@qq.com>
2019-05-29 14:21:45 +0800

    [fixed]popup button bug

--- components/popup-button/popup-button.js
+++ components/popup-button/popup-button.js
@@ -1,4 +1,5 @@
-const BASE_PATH = '../../images/popup-button/'
+const BASE_PATH = "../../images/popup-button/";
+var app = getApp();
 Component({
   properties: {},
   data: {
@@ -14,67 +15,104 @@ Component({
         index: 1,
         icon: `${BASE_PATH}search.png`,
         animation: {},
-        tap: 'test'
+        tap: "search"
       },
       {
         index: 2,
         icon: `${BASE_PATH}location.png`,
         animation: {},
-        tap: 'test'
+        tap: "location"
       },
       {
         index: 3,
         icon: `${BASE_PATH}chat.png`,
         animation: {},
-        tap: 'test'
+        tap: "chat"
       }
     ],
     show: false
   },
-  onLoad() {
-    this.expend = false
-  },
-  onReady: function () {
-    this.animation = wx.createAnimation()
+  lifetimes: {
+    created: function() {
+      this.expend = false;
+    }
   },
   methods: {
-    test() {
-      debugger
+    location() {
+      wx.navigateTo({
+        url: "/pages/near-shop/near-shop"
+      });
     },
-    rotate: function () {
-      let _ = this
-      this.expend = !this.expend
-      let animation = wx.createAnimation({
-        duration: 500,
-        timingFunction: 'ease',
-        delay: 500
-      })
-      animation.rotate(_.expend ? 0 : -45).step()
-      _.setData({ animation: animation.export() })
-      this.move(-30, -40, 1)
-      this.move(-55, 0, 2)
-      this.move(-30, 40, 3)
+    search() {
+      wx.navigateTo({
+        url: "/pages/search/search"
+      });
+    },
+    chat() {
+      app.tokenCheck(() => {
+        app.MainApi.userService({
+          data: {
+            token: app.LM.token,
+            productId: ""
+          },
+          other: {
+            isShowLoad: true
+          }
+        }).then(res => {
+          console.log(res);
+          // debugger
+          if (res.code == 0) {
+            const data = res.data || {};
+            wx.navigateTo({
+              url: `/pages/wap/wap?url=${data.serviceUrl || ""}`
+            });
+          } else if (res.message === "还没有关注导购") {
+            // debugger
+            wx.navigateTo({
+              url: "/pages/shopping-guide/shopping-guide?redirect=1"
+            });
+          }
+        });
+      });
+    },
+    rotate: function() {
+      this.expend = !this.expend;
+      var animation = wx.createAnimation({
+        duration: 300,
+        timingFunction: "ease",
+        delay: 200
+      });
+      let angle = this.expend ? -45 : 0;
+      animation.rotate(angle).step();
+      this.setData({
+        animation: animation.export()
+      });
+      this.move(-30, -45, 1);
+      this.move(-65, 0, 2);
+      this.move(-30, 45, 3);
     },
     move(x, y, index) {
-      let _ = this
+      let _ = this;
       let animation = wx.createAnimation({
-        duration: 500,
-        timingFunction: 'ease',
+        duration: 300,
+        timingFunction: "ease",
         delay: 0
-      })
-      let paramOne = _.expend ? 0 : x
-      let paramTwo = _.expend ? 0 : y
-      animation.translate(paramOne, paramTwo).step()
-      if (_.expend) {
-        _.setData({
-          show: false
-        })
-      } else {
-        _.setData({
+      });
+      let paramOne = this.expend ? x : 0;
+      let paramTwo = this.expend ? y : 0;
+      animation.translate(paramOne, paramTwo).step();
+      if (this.expend) {
+        this.setData({
           show: true
-        })
+        });
+      } else {
+        setTimeout(() => {
+          this.setData({
+            show: false
+          });
+        }, 200);
       }
-      _.setData({ [`items[${index - 1}].animation`]: animation.export() })
+      this.setData({ [`items[${index - 1}].animation`]: animation.export() });
     }
   }
-})
+});
