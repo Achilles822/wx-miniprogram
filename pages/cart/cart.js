02b260b27f36b2a73efcbe907dbcda2e7042ef76
achilles <achilles822@icloud.com>
2019-06-26 12:16:51 +0800

    [feat]加入购物车动画（贝塞尔曲线）

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -2,14 +2,21 @@
 var app = getApp();
 
 Page({
-  /**
-   * 页面的初始数据
-   */
   data: {
     showPop: false,
-    animationData: {}
+    animationData: {},
+    ballDisplay: false,
+    top: 0,
+    left: 0,
+    originTop: 0,
+    originLeft: 0
   },
-
+  onShow() {
+    // 获取小球最开始的位置
+    this.initBallPos();
+    this.start = false;
+  },
+  onReady() {},
   // 显示底部弹层
   showModal: function() {
     var _this = this;
@@ -58,5 +65,56 @@ Page({
       }.bind(this),
       200
     );
+  },
+  // 加入购物车动画
+  addCart() {
+    let that = this;
+    // 禁止动画多次触发
+    if (this.start) {
+      return;
+    }
+    this.start = true;
+    this.setData({
+      ballDisplay: true
+    });
+    // 获取小球终点位置
+    this.getRects(".after-position").then(rect => {
+      this.setData({
+        top: `${rect.top + 16}px`,
+        left: `${rect.left + 16}px`
+      });
+      // 延时跟动画时长一致，飞完隐藏掉，再把小球重置到初始位置。
+      let { originLeft, originTop } = this.data;
+      setTimeout(() => {
+        that.setData({
+          ballDisplay: false,
+          top: originTop,
+          left: originLeft
+        });
+        that.start = false;
+      }, 1000);
+    });
+  },
+  getRects(cls) {
+    return new Promise((resolve, reject) => {
+      wx.createSelectorQuery()
+        .in(this)
+        .select(cls)
+        .boundingClientRect(function(rect) {
+          console.log(rect);
+          resolve(rect);
+        })
+        .exec();
+    });
+  },
+  initBallPos() {
+    this.getRects(".before-position").then(rect => {
+      this.setData({
+        top: `${rect.top + 5}px`,
+        left: `${rect.left + 30}px`,
+        originTop: `${rect.top + 5}px`,
+        originLeft: `${rect.left + 30}px`
+      });
+    });
   }
 });
